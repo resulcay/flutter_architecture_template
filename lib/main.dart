04863dee634524dd19e316ea634641b6e1a77f87
resulcay <resulcayop@gmail.com>
2024-03-07 19:22:07 +0300

    test change

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'PureBlog',
+      title: 'Pure Blog',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
